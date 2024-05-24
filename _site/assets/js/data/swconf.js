f86bea49e909831c2eff070a1943aa306f24b1aa
yufu.lly <yufu.lly@antfin.com>
2024-05-24 18:09:49 +0800

    add hello world

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716540879',resources: [
+    cacheName: 'chirpy-1716545298',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
